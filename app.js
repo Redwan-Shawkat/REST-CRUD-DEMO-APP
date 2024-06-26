00e297a14bb8e459b4c800488b22744142bde353
Redwan Shawkat <redwanshawkat@gmail.com>
2024-06-26 22:32:18 +0600

    File Upload

---
+++ app.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const fs = require('fs');
+const app = express();
+
+// -----> Miiddleware 
+app.use(express.json());
+
+const port = 8080;
+
+app.get('/', (req, res) => {
+     console.log(req);
+     return res.send("Home Page");
+})
+
+app.get('/books', (req, res) => {
+     const data = fs.readFileSync('./books.json', 'utf-8');
+     return res.send(data);
+})
+
+app.get("/books/:bookId/user/:userId", (req, res) => {
+  const data = JSON.parse(fs.readFileSync("./books.json", "utf-8"));
+  console.log('Params: ', req.params);
+  console.log("Query Strings", req.query);
+  const bookId = Number(req.params.bookId);
+  return res.send(
+    data?.find((item) => item.id === bookId) || { message: "No books found!" }
+  );
+});
+
+app.post("/books", (req, res) => {
+  const data = JSON.parse(fs.readFileSync("./books.json", "utf-8"));
+  const book = req.body;
+  data.push(book);
+
+  fs.writeFileSync("./books.json", JSON.stringify(data));
+  return res.send({
+    data: { ...book },
+    message: "Book saved successfully!",
+  });
+});
+
+app.put("/books/:bookId", (req,res) => {
+	const bookId = Number(req.params.bookId); 
+	const updateBook = req.body; 
+	const data = JSON.parse(fs.readFileSync("./books.json", "utf-8"));
+	const updateData = data?.map(item => {
+	if (item.id == bookId) {
+		return {
+			... updateBook,
+		}
+	}
+	return item;
+})
+fs.writeFileSync("./books.json",JSON.stringify(updateData));
+return res.send({message: 'Book is updated successfully'})
+})
+
+app.delete('/books/:bookId', (req,res) => {
+	const bookId = Number (req.params.bookId);
+	const data = JSON.parse(fs.readFileSync("./books.json","utf-8"));
+	const updateData = data?.filter((item) => item.id != bookId);
+	fs.writeFileSync("./books.json",JSON.stringify(updateData));
+	return res.send({message: "Book is deleted successfully!" })
+})
+
+app.listen(port, () => {
+     console.log(`App is running at ${port}`);
+})
