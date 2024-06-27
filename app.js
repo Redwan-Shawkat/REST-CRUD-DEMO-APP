1af819b05b209c56be0fdf8d8200279cbddcd4de
Redwan Shawkat <redwanshawkat@gmail.com>
2024-06-27 20:17:21 +0600

    using route

--- app.js
+++ app.js
@@ -12,54 +12,63 @@ app.get('/', (req, res) => {
      return res.send("Home Page");
 })
 
-app.get('/books', (req, res) => {
-     const data = fs.readFileSync('./books.json', 'utf-8');
-     return res.send(data);
-})
+app
+  .route("/books")
+  // -----> GET 
+  .get( (req, res) => {
+    const data = fs.readFileSync("./books.json", "utf-8");
+    return res.send(data);
+  })
+  // -----> POST
+  .post((req, res) => {
+    const data = JSON.parse(fs.readFileSync("./books.json", "utf-8"));
+    const book = req.body;
+    data.push(book);
 
-app.get("/books/:bookId", (req, res) => {
-  const data = JSON.parse(fs.readFileSync("./books.json", "utf-8"));
-  const bookId = Number(req.params.bookId);
-  return res.send(
-    data?.find((item) => item.id === bookId) || { message: "No books found!" }
-  );
-});
-
-app.post("/books", (req, res) => {
-  const data = JSON.parse(fs.readFileSync("./books.json", "utf-8"));
-  const book = req.body;
-  data.push(book);
-
-  fs.writeFileSync("./books.json", JSON.stringify(data));
-  return res.send({
-    data: { ...book },
-    message: "Book saved successfully!",
+    fs.writeFileSync("./books.json", JSON.stringify(data));
+    return res.send({
+      data: { ...book },
+      message: "Book saved successfully!",
+    });
   });
-});
 
-app.put("/books/:bookId", (req,res) => {
-	const bookId = Number(req.params.bookId); 
-	const updateBook = req.body; 
-	const data = JSON.parse(fs.readFileSync("./books.json", "utf-8"));
-	const updateData = data?.map(item => {
-	if (item.id == bookId) {
-		return {
-			... updateBook,
-		}
-	}
-	return item;
-})
-fs.writeFileSync("./books.json",JSON.stringify(updateData));
-return res.send({message: 'Book is updated successfully'})
-})
+  app
+    .route("/books/:bookId")
+    // -----> GET
+    .get((req, res) => {
+      const data = JSON.parse(fs.readFileSync("./books.json", "utf-8"));
+      const bookId = Number(req.params.bookId);
+      return res.send(
+        data?.find((item) => item.id === bookId) || {
+          message: "No books found!",
+        }
+      );
+    })
+    // -----> PUT
+    .put((req, res) => {
+      const bookId = Number(req.params.bookId);
+      const updateBook = req.body;
+      const data = JSON.parse(fs.readFileSync("./books.json", "utf-8"));
+      const updateData = data?.map((item) => {
+        if (item.id == bookId) {
+          return {
+            ...updateBook,
+          };
+        }
+        return item;
+      });
+      fs.writeFileSync("./books.json", JSON.stringify(updateData));
+      return res.send({ message: "Book is updated successfully" });
+    })
+    // -----> DELETE 
+    .delete( (req, res) => {
+      const bookId = Number(req.params.bookId);
+      const data = JSON.parse(fs.readFileSync("./books.json", "utf-8"));
+      const updateData = data?.filter((item) => item.id != bookId);
+      fs.writeFileSync("./books.json", JSON.stringify(updateData));
+      return res.send({ message: "Book is deleted successfully!" });
+    });
 
-app.delete('/books/:bookId', (req,res) => {
-	const bookId = Number (req.params.bookId);
-	const data = JSON.parse(fs.readFileSync("./books.json","utf-8"));
-	const updateData = data?.filter((item) => item.id != bookId);
-	fs.writeFileSync("./books.json",JSON.stringify(updateData));
-	return res.send({message: "Book is deleted successfully!" })
-})
 
 app.listen(port, () => {
      console.log(`App is running at ${port}`);
